ef4e358efda8b4b3932c4c0226f79785663b0139
bozhenakv <bkvartuk@gmail.com>
2025-04-05 23:01:48 +0300

    Inicializacia premennej max, zapis posledneho prvku pola in do nej, inicializacia pocitadla vo funkcii vyber_prave_maxima, zapis prveho maxima do pola out

---
+++ 7.5.27.pravemax.bozhenakv.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+unsigned int vyber_prave_maxima(int *in, unsigned int l, int *out) {
+	int max = in[l-1];
+	unsigned int count = 0;
+	out[count]=max;
+	count++; 
+}
+
+int main(){
+	int max = in[l-1];
+	unsigned int count = 0;
+	out[count]=max;
+	count++;
+	
+}
