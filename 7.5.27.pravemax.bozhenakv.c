47f33da3001b5027cc03920e34c10b45a9aeb146
bozhenakv <bkvartuk@gmail.com>
2025-04-05 23:49:42 +0300

    Pridanie navratovej hodnoty funkcii (count - pocet najdenych prvkov s hladanou vlastnostou), pridanie chybajucej bodkociarky po out[count]=max, pridanie overenia ze pole nie je prazdne. V main funkcii bola zadefinovana l - pocet prvkov vstupneho pola a vstupne pole z  l, implementovana dynamicka alokacia pamate pre pole out a zavolanie funkcie vyber_prave_maxima pre spracovanie vstupneho pola

--- 7.5.27.pravemax.bozhenakv.c
+++ 7.5.27.pravemax.bozhenakv.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 unsigned int vyber_prave_maxima(int *in, unsigned int l, int *out) {
+	if (in == NULL){
+		return 0;
+	}
 	int max = in[l-1];
 	unsigned int count = 0;
 	out[count]=max;
@@ -8,12 +11,15 @@ unsigned int vyber_prave_maxima(int *in, unsigned int l, int *out) {
 	for (int i=l-2; i>=0; i--){
 		if(in[i]>max){
 			max=in[i];
-			out[count]=max
+			out[count]=max;
 			count++;
 		}
 	}
+	return count;
 }
 
 int main(){
+	int in[l]={2,5,3,9,7,4,6,3,1};
+	int out[];
+	unsigned int vysledok = vyber_prave_maxima(in, l, out);
 	
-}
