eaaf7820d985cea24dde8ef6450d0b8f2c3c32c7
bozhenakv <bkvartuk@gmail.com>
2025-04-06 01:58:32 +0300

    Vytlacenie poctu pravych maximov a ich hodnot z pola out (pomocou pouzitia cyklusa for, ktory vypisuje kazdy prvok out), pridanie free, ktore uvolni pamat alokovanu pre pole out, pridanie dalsej kontroly, ci velkost pola in (l) nie je nulova, a tiez oprava chyby v inicializacii pola in, pretoze jeho velkostou nemoze byt premenna

--- 7.5.27.pravemax.bozhenakv.c
+++ 7.5.27.pravemax.bozhenakv.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 unsigned int vyber_prave_maxima(int *in, unsigned int l, int *out) {
-	if (in == NULL){
+	if (in == NULL || l == 0){
 		return 0;
 	}
 	int max = in[l-1];
@@ -19,7 +19,17 @@ unsigned int vyber_prave_maxima(int *in, unsigned int l, int *out) {
 }
 
 int main(){
-	int in[l]={2,5,3,9,7,4,6,3,1};
-	int out[];
+	unsigned int l = 9;
+	int in[9]={2,5,3,9,7,4,6,3,1};
+	int *out=(int*)malloc(l*sizeof(int));
 	unsigned int vysledok = vyber_prave_maxima(in, l, out);
+	
+    printf("Pocet najdenych pravych maximov: %u\n", vysledok);
+    printf("Najdene prave maximy:\n");
+    for (int i=0; i<vysledok; i++){
+    	printf("%d", out[i]);
+	}
+	free(out);
+	return 0;
+}
 	
