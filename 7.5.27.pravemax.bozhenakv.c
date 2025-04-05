9eabdf31bfdae93e949428ea3c17fde7e2044f8b
bozhenakv <bkvartuk@gmail.com>
2025-04-06 02:29:30 +0300

    Pridanie funkcie reverse a jej pouzivanie v maine pred vytlacenim pola out na reverzovanie tohto pola, aby najdene prvky boli ulozene v out v tom istom poradi v akom za sebou nasleduju v in

--- 7.5.27.pravemax.bozhenakv.c
+++ 7.5.27.pravemax.bozhenakv.c
@@ -18,6 +18,17 @@ unsigned int vyber_prave_maxima(int *in, unsigned int l, int *out) {
 	return count;
 }
 
+void reverse_a(int *a, unsigned int l){
+	unsigned int i=0, j=l-1;
+	do {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+		i++; 
+		j--;
+	} while (i < j);
+}
+
 int main(){
 	unsigned int l = 9;
 	int in[9]={2,5,3,9,7,4,6,3,1};
@@ -25,6 +36,7 @@ int main(){
 	unsigned int vysledok = vyber_prave_maxima(in, l, out);
 	
     printf("Pocet najdenych pravych maximov: %u\n", vysledok);
+    reverse_a(out, vysledok);
     printf("Najdene prave maximy:\n");
     for (int i=0; i<vysledok; i++){
     	printf("%d", out[i]);
