d6f6a29a5231e480a2049faec874a0291ee63598
bozhenakv <bkvartuk@gmail.com>
2025-04-05 23:24:29 +0300

    Pridany cyklus for na prechadzanie po prvkoch pola od konca k zaciatku, porovnanie hodnoty prvka z poslednym maximom a po splneni potrebnoj podmienky, priradenia prvku do maxima a zapis noveho maxima do pola out

--- 7.5.27.pravemax.bozhenakv.c
+++ 7.5.27.pravemax.bozhenakv.c
@@ -4,13 +4,16 @@ unsigned int vyber_prave_maxima(int *in, unsigned int l, int *out) {
 	int max = in[l-1];
 	unsigned int count = 0;
 	out[count]=max;
-	count++; 
+	count++;
+	for (int i=l-2; i>=0; i--){
+		if(in[i]>max){
+			max=in[i];
+			out[count]=max
+			count++;
+		}
+	}
 }
 
 int main(){
-	int max = in[l-1];
-	unsigned int count = 0;
-	out[count]=max;
-	count++;
 	
 }
